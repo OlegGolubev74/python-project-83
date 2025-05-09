c85e0666cfbcb9a9c361afc40c047391d02bc720
oleg <oleg.golubev74@gmail.com>
2025-05-09 22:12:41 +0300

    step 2 ref 2

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
+
 
 from dotenv import load_dotenv
 
@@ -13,6 +13,13 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/analyze", methods=['POST'])
+def analyze():
+    url = request.form.get('url')  # Получаем URL из формы
+    # Здесь будет логика анализа URL (пока просто редирект)
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
